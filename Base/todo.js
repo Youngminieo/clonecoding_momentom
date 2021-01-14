087baceaeb0fae872e22acac307163b0870d638c
WI Young Min <youngminieo1005@gmail.com>
2021-01-14 21:02:26 +0900

    to-do ver 2 update

--- Base/todo.js
+++ Base/todo.js
@@ -3,20 +3,39 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
+// TODOS_LS = localStorage에 키값
+// toDos는 TODOS_LS 키값에 해당하는 object array 
 const TODOS_LS = "toDos";
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+}
 
 // 5. li , button(삭제버튼) , span 을 createElement로 만든다. 
 // span에 매개변수로 들어온 text(즉, input-text에서 입력된 텍스트)를 반영하고
 // li 에 button과 span을 넣어주고, 마지막에 toDoList(html의 ul)에 넣어준다.
+// 우리가 할거는 localStorage에 TODOS_LS키에 해당하는 object객체배열을 통해, 투두리스트를 불러오는거다. (새로고침해도 그대로)
+// 이는, 매번, 추가되는 투두리스트가 추가될때마다 브라우저에만 보여주는게 아니라, localStorage에 배열에 추가된 정보를 계속 업데이트하는 식이여야한다.
+
 function printToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+
+    delBtn.innerText = "❌";
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId; 
     toDoList.appendChild(li);
+    const toDoObj = {
+        text : text,
+        id : newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 // 4. 이벤트가 들어온 상태를 유지한다.
@@ -30,11 +49,17 @@ function handleSubmit(event){
 }
 
 // 2. localStorage에 TODOS_LS 변수안에 키에 해당하는 value를 가져온다.
-// 값이 비어있지 않을 경우만 생각
-function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null){
+// 값이 비어있지 않을 경우, 현재 localStorage에 TODOS_LS 변수에있는 키에, 해당하는 값을 가져온다.
+// 그 값이 비어있지 않은경우, JSON.parse()를 이용해서 string데이터 -> object데이터로 대입해놓는다.
+// parsedToDos 가 object array임으로, foreach() 함수 적용이 가능하다.
 
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            printToDo(toDo.text);
+        });
     }
 }
 
